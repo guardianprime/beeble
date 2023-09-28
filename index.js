38c34fb86b7c4d449c07d3494921b799738ab41b
Guardianprime <guardianacer@outlook.com>
2023-09-28 10:36:29 +0100

    The html boiler plate for a mockup website

---
+++ index.js
@@ -0,0 +1,27 @@
+ 
+//Listening/waitig for the document to be ready
+document.addEventListener('DOMContentLoaded', () => {
+ 
+    const selectDrop = document.querySelector('#countries');
+    // const selectDrop = document.getElementById('countries');
+     
+     
+    fetch('http://restcountries.eu/rest/v2/all').then(res => {
+    return res.json();
+    }).then(data => {
+    let output = "";
+    data.forEach(country => {
+      output += `
+        <option value="${country.name}">${country.name}  </option>`;
+    })
+     
+    selectDrop.innerHTML = output;
+     
+    }).catch(err => {
+    console.log(err);
+    })
+     
+     
+    });
+        
+     
